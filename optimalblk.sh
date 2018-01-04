e3a92a3748dcc3b2b7dfec15c1ed8e01da29153e
EmilieTrouillard <s172727@student.dtu.dk>
2018-01-04 22:59:49 +0100

    deletes the intermediary file

--- optimalblk.sh
+++ optimalblk.sh
@@ -1,4 +1,5 @@
-for((bs=2; bs<100; bs += 3)); do matmult_c.gcc blk $1 $1 $1 $bs ; done > optimalblk$1.txt
-awk '{r = $6; s = $2}; {print r, s}' ./optimalblk$1.txt > optimalblk$1.dat
+for((bs=2; bs<100; bs += 3)); do matmult_c.gcc blk $1 $1 $1 $bs ; done > _optimalblk$1.dat
+awk '{r = $6; s = $2}; {print r, s}' ./_optimalblk$1.dat > optimalblk$1.dat
+rm _optimalblk$1.dat
 
 
