cbd8666961a0f7962d7a9413f8ac9e868137c537
Helge <helge@hatteland.com>
2018-01-04 13:48:40 +0100

    Changed header

--- matmult.cpp
+++ matmult.cpp
@@ -1,6 +1,18 @@
 
 extern "C" {
 #include <cblas.h>
+
+void matmult_nat(int m,int n,int k,double **A,double **B,double **C);
+void matmult_lib(int m,int n,int k,double **A,double **B,double **C);
+
+void matmult_mkn(int m,int n,int k,double **A,double **B,double **C);
+void matmult_mnk(int m,int n,int k,double **A,double **B,double **C);
+void matmult_kmn(int m,int n,int k,double **A,double **B,double **C);
+void matmult_knm(int m,int n,int k,double **A,double **B,double **C);
+void matmult_nmk(int m,int n,int k,double **A,double **B,double **C);
+void matmult_nkm(int m,int n,int k,double **A,double **B,double **C);
+
+void matmult_blk(int m,int n,int k,double **A,double **B,double **C, int bs);
 }
 
 #define FOR_i_TO_m for (i = 0; i < m; i++)
