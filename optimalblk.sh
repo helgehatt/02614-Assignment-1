a6b84862df55acc3b236b51eabc9f6b903de6f2c
EmilieTrouillard <s172727@student.dtu.dk>
2018-01-04 21:42:09 +0100

    version that creates a .dat file ready to be plotted with gnuplot

--- optimalblk.sh
+++ optimalblk.sh
@@ -1,1 +1,4 @@
-for((bs=1; bs<100; bs += 5)); do matmult_c.gcc blk $1 $1 $1 $bs ; done > optimalblk$1.txt
+for((bs=2; bs<100; bs += 3)); do matmult_c.gcc blk $1 $1 $1 $bs ; done > optimalblk$1.txt
+awk '{r = $6; s = $2}; {print r, s}' ./optimalblk$1.txt > optimalblk$1.dat
+
+
