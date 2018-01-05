b58cd2f97881b5c803af0830aff216a9ef214951
EmilieTrouillard <s172727@student.dtu.dk>
2018-01-05 11:27:13 +0100

    script that plots the results of 1 to 3 sizes of matrices

--- optimalblk.sh
+++ optimalblk.sh
@@ -1,5 +1,19 @@
-for((bs=2; bs<100; bs += 3)); do matmult_c.gcc blk $1 $1 $1 $bs ; done > _optimalblk$1.dat
+for i in $1 $2 $3; do(
+for((bs=10; bs<150; bs += 3)); do matmult_c.gcc blk $i $i $i $bs ; done > _optimalblk$i.dat); done
 awk '{r = $6; s = $2}; {print r, s}' ./_optimalblk$1.dat > optimalblk$1.dat
 rm _optimalblk$1.dat
+awk '{r = $6; s = $2}; {print r, s}' ./_optimalblk$2.dat > optimalblk$2.dat
+rm _optimalblk$2.dat
+awk '{r = $6; s = $2}; {print r, s}' ./_optimalblk$3.dat > optimalblk$3.dat
+rm _optimalblk$3.dat
+gnuplot -persist <<-EOFMarker
+	set terminal postscript eps enhanced color font 'Helvetica,10'
+	set xlabel "Block_size"
+	set ylabel "MFlops"
+	set output "testplot$1$2$3.eps"
+	plot "optimalblk$1.dat" w lp title 'array size = $1', "optimalblk$2.dat" w lp title 'array size = $2', "optimalblk$3.dat" w lp title 'array size = $3'
+EOFMarker
+
+
 
 
